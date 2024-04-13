61a0abfe8ee98153b6be24eb6511da9c1aee1610
Sheila Murugi <tmurugi49@gmail.com>
2024-04-13 04:40:47 +0300

    Modification

--- src/utils.py
+++ src/utils.py
@@ -1,182 +1,275 @@
-import os
-import sys
-import glob
-import json
-import datetime
-from collections import Counter
-from collections import Counter
 
 import pandas as pd
-from matplotlib import pyplot as plt
-import seaborn as sns
+# import seaborn as sns
+
+import nltk;
+from nltk.tokenize import word_tokenize
+from nltk import pos_tag,ne_chunk
+from collections import Counter
+from multiprocessing import Pool
+from tqdm import tqdm
+
+#imports for task-2
+from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import TfidfVectorizer
+from scipy.spatial.distance import cosine
+
+#from bertopic import BERTopic
+import re
+from nltk.stem import WordNetLemmatizer
+
+nltk.download('stopwords')
+nltk.download('wordnet')
 
 
-def break_combined_weeks(combined_weeks):
+
+
+def find_top_websites(data,url_column='url',top=10):
     """
-    Breaks combined weeks into separate weeks.
+        this function will get the top [top] websites with highest article counts
+    """
+    data['domain'] = data[url_column].apply(lambda x: x.split('/')[2])
+
+    #count occurences of each domain
+    domain_counts = data['domain'].value_counts()
+
+    top_domains = domain_counts.head(top)
+    return top_domains
+
+def find_high_traffic_websites(data,top=10):
+    """
+    this function will return websites with high reference ips(assuming the ips are the number of traffic)
+    this should include/join the dataset in news
+    """
+
+    traffic_per_domain = data.groupby(['Domain'])['RefIPs'].sum()
+    traffic_per_domain = traffic_per_domain.sort_values(ascending=False)
+    return traffic_per_domain.head(top)
+
+def find_countries_with_most_media(data,top=10):
+    """
+    this function will return the top countires with the most media outlets
+    it simply groups the data by the country field and counts them
+    """
+    media_per_country = data['Country'].value_counts()
+    media_per_country = media_per_country.sort_values(ascending=False)
+    return media_per_country.head(top)
+
+
+"""
+the following set of functions will create
+analysis function using nltk(an NLP libraries) to calculate
+how many news stories are about a specific countries
+"""
+#first download required nltk packages
+
+def download_nltk_resources():
+    nltk.download('punkt')
+    nltk.download('averaged_perceptron_tagger')
+    nltk.download('maxent_ne_chunker')
+    nltk.download('words')
+
+count = 0
+
+def extract_countries_from_article_content(article):
+    index,row = article
+    text = row['content']
+    #tokenize every text into words
+    words = word_tokenize(text)
+    #part of speech tagging; this means tag every word; with noun,verb ...
+    tagged_words = pos_tag(words)
+    #get named entities from the tagged lists
+    named_entities = ne_chunk(tagged_words)
+    #GPE stands for 'Geopolitical Entity' in our case country name
+    countries = [chunk[0] for chunk in list(named_entities) if hasattr(chunk,'label') and chunk.label() == 'GPE']
+
     
-    Args:
-        combined_weeks: list of tuples of weeks to combine
+    return countries
+
+def find_popular_articles(popular_countries_data):
+    print('downloading nltk resources ...')
+    download_nltk_resources()
+    print('finished downloading resources ...')
+    print('loading data ...')
+    df = popular_countries_data
+    print('starting processing this might take a while ...')
+    #since we have a lot of data we need to parallize the process
+
+    # Maximum number of rows to process
+    max_rows = len(df)
+    print(f'max rows is: {max_rows}')
+    processed_count = 0
+    # Apply function to each article in parallel with tqdm for progress bar
+    with Pool() as pool:
+        results = []
+        for countries in tqdm(pool.imap(extract_countries_from_article_content, df.iterrows()), total=len(df)):
+            # Append the results
+            results.append(countries)
+            
+            # Increment processed_count
+            processed_count += 1
+            
+            # Check if maximum number of rows processed
+            if processed_count >= max_rows:
+                print("Maximum number of rows processed. Stopping pool.")
+                break
+    print('done processing!')
+    # Flatten the list of results
+    all_countries = [country for countries in results for country in countries]
+    
+   
+
+    # Count occurrences of each country
+    print("debug printing count...")
+    country_counts = Counter(all_countries)
+    print(country_counts.most_common(3))
+    return country_counts.most_common(10)
+
+
+def webiste_sentiment(data):
+    sentiment_counts = data.groupby(['source_name','title_sentiment']).size().unstack(fill_value = 0)
+    return sentiment_counts
+
+def website_sentiment_distribution(data):
+    sentiment_counts=data.groupby(['source_name','title_sentiment']).size().unstack(fill_value = 0)
+    sentiment_counts['Total'] = sentiment_counts.sum(axis=1)
+
+    # Calculate mean and median sentiment counts for each domain
+    sentiment_counts['Mean'] = sentiment_counts[['Positive', 'Neutral', 'Negative']].mean(axis=1)
+    sentiment_counts['Median'] = sentiment_counts[['Positive', 'Neutral', 'Negative']].median(axis=1)
+
+    # Display the sentiment counts along with mean and median
+    print("Sentiment counts with mean and median:")
+    print(sentiment_counts)
+    return sentiment_counts
+
+
+#task two topic modeling and sentiment analysis
+
+
+def keyword_extraction_and_analysis(news_data):
+
+    """
+        this function will perform key word extraction usig tf-idf
+        used 10 keyword accross header and title
+        and it needs to occur at least once on both header and content to be considered
+
+        min_words_threshold will limit the analysis for articles with at least 5 words
         
-    Returns:
-        tuple of lists of weeks to be treated as plus one and minus one
     """
-    plus_one_week = []
-    minus_one_week = []
+    #nltk.download('stopwords')
+    # Define stop words (English in this example)
+    stop_words = set(stopwords.words('english'))
 
-    for week in combined_weeks:
-        if week[0] < week[1]:
-            plus_one_week.append(week[0])
-            minus_one_week.append(week[1])
+    # Create TF-IDF vectorizer
+    vectorizer = TfidfVectorizer(max_features=10, min_df=1)  # Adjust max_features for desired number of keywords
+
+    # Initialize lists to store keywords and similarities
+    title_keywords_list = []
+    content_keywords_list = []
+    similarity_list = []
+
+    MIN_WORDS_THRESHOLD = 5  # Adjust as needed
+    article_count = 0 #perform for the first 10 articles for now
+    # Process each news item
+    for index, row in news_data.iterrows():
+        # Preprocess text (lowercase, remove punctuation, tokenize)
+
+        if(article_count == 101): break
+        
+        title_text = row['title']
+       
+        # Preprocess text (lowercase, remove punctuation, tokenize)
+        processed_title = [word.lower() for word in word_tokenize(title_text) if word.lower() not in stop_words and word.isalpha()]
+        content_text = row['content']
+       
+        processed_content = [word.lower() for word in word_tokenize(content_text) if word.lower() not in stop_words and word.isalpha()]
+
+        if len(processed_title) < MIN_WORDS_THRESHOLD and len(processed_content) < MIN_WORDS_THRESHOLD:
+            continue
+
+        # Combine title and content for TF-IDF analysis
+        combined_text = ' '.join(processed_title + processed_content)
+       
+        # Fit vectorizer to combined text
+        vectorizer.fit([combined_text])
+
+        # Extract TF-IDF scores for the current article
+        tfidf_scores = vectorizer.transform([combined_text]).toarray()[0]
+        
+        
+        
+        # Get feature names (words)
+        feature_names = vectorizer.get_feature_names_out()
+
+        # Sort keywords by TF-IDF scores (descending order) and select top 5
+        top_keywords_title = [keyword for keyword, _ in sorted(zip(feature_names, tfidf_scores), key=lambda x: x[1], reverse=True)[:5]]
+        top_keywords_content = [keyword for keyword, _ in sorted(zip(feature_names, tfidf_scores), key=lambda x: x[1], reverse=True)[5:10]]
+
+       
+        if(len(tfidf_scores) >= 10):
+
+             # Append top keywords to lists
+            title_keywords_list.append(top_keywords_title)
+            content_keywords_list.append(top_keywords_content)
+
+            # Calculate cosine similarity between title and content keywords
+            title_tfidf_scores = tfidf_scores[:5]
+            content_tfidf_scores = tfidf_scores[5:10]
+
+            
+
+            similarity = 1 - cosine(title_tfidf_scores, content_tfidf_scores)
+            
+            similarity_list.append(similarity)
+            article_count = article_count+1
         else:
-            minus_one_week.append(week[0])
-            plus_one_week.append(week[1])
-
-    return plus_one_week, minus_one_week
-
-def get_msgs_df_info(df):
-    msgs_count_dict = df.user.value_counts().to_dict()
-    replies_count_dict = dict(Counter([u for r in df.replies if r != None for u in r]))
-    mentions_count_dict = dict(Counter([u for m in df.mentions if m != None for u in m]))
-    links_count_dict = df.groupby("user").link_count.sum().to_dict()
-    return msgs_count_dict, replies_count_dict, mentions_count_dict, links_count_dict
+            print('can not caluclate similarty on unbalanced keywords')
+        
+        
+    return title_keywords_list, content_keywords_list, similarity_list
 
 
-
-def get_messages_dict(msgs):
-    msg_list = {
-            "msg_id":[],
-            "text":[],
-            "attachments":[],
-            "user":[],
-            "mentions":[],
-            "emojis":[],
-            "reactions":[],
-            "replies":[],
-            "replies_to":[],
-            "ts":[],
-            "links":[],
-            "link_count":[]
-            }
-
-
-    for msg in msgs:
-        if "subtype" not in msg:
-            try:
-                msg_list["msg_id"].append(msg["client_msg_id"])
-            except:
-                msg_list["msg_id"].append(None)
-            
-            msg_list["text"].append(msg["text"])
-            msg_list["user"].append(msg["user"])
-            msg_list["ts"].append(msg["ts"])
-            
-            if "reactions" in msg:
-                msg_list["reactions"].append(msg["reactions"])
-            else:
-                msg_list["reactions"].append(None)
-
-            if "parent_user_id" in msg:
-                msg_list["replies_to"].append(msg["ts"])
-            else:
-                msg_list["replies_to"].append(None)
-
-            if "thread_ts" in msg and "reply_users" in msg:
-                msg_list["replies"].append(msg["replies"])
-            else:
-                msg_list["replies"].append(None)
-            
-            if "blocks" in msg:
-                emoji_list = []
-                mention_list = []
-                link_count = 0
-                links = []
-                
-                for blk in msg["blocks"]:
-                    if "elements" in blk:
-                        for elm in blk["elements"]:
-                            if "elements" in elm:
-                                for elm_ in elm["elements"]:
-                                    
-                                    if "type" in elm_:
-                                        if elm_["type"] == "emoji":
-                                            emoji_list.append(elm_["name"])
-
-                                        if elm_["type"] == "user":
-                                            mention_list.append(elm_["user_id"])
-                                        
-                                        if elm_["type"] == "link":
-                                            link_count += 1
-                                            links.append(elm_["url"])
-
-
-                msg_list["emojis"].append(emoji_list)
-                msg_list["mentions"].append(mention_list)
-                msg_list["links"].append(links)
-                msg_list["link_count"].append(link_count)
-            else:
-                msg_list["emojis"].append(None)
-                msg_list["mentions"].append(None)
-                msg_list["links"].append(None)
-                msg_list["link_count"].append(0)
+#bert topic modeling
+# Define your own categories
+own_categories = {
+    0: 'Breaking News',
+    1: 'Politics',
+    2: 'World News',
+    3: 'Business/Finance',
+    4: 'Technology',
+    5: 'Science',
+    6: 'Health',
+    7: 'Entertainment',
+    8: 'Sports',
+    9: 'Environment',
+    10: 'Crime',
+    11: 'Education',
+    12: 'Weather',
     
-    return msg_list
+}
 
-def from_msg_get_replies(msg):
-    replies = []
-    if "thread_ts" in msg and "replies" in msg:
-        try:
-            for reply in msg["replies"]:
-                reply["thread_ts"] = msg["thread_ts"]
-                reply["message_id"] = msg["client_msg_id"]
-                replies.append(reply)
-        except:
-            pass
-    return replies
+def clean_text(text):
+    clean_text = re.sub('<.*?>','',text)
+    clean_text = re.sub(r'[^\w\s]','',text)
+    return clean_text
 
-def msgs_to_df(msgs):
-    msg_list = get_messages_dict(msgs)
-    df = pd.DataFrame(msg_list)
-    return df
+def preprocess_text(text):
+    #tokenization and removal of stop words
+    stop_words = set(stopwords.words('english'))
+    words = text.lower().split()
+    words = [word for word in text if word not in stop_words]
 
-def process_msgs(msg):
-    '''
-    select important columns from the message
-    '''
+    #lemmetize : meaning reducing the word to its root form
+    lemmetizer = WordNetLemmatizer()
+    words = [lemmetizer.lemmatize(word) for word in words]
+    return ' '.join(words)
 
-    keys = ["client_msg_id", "type", "text", "user", "ts", "team", 
-            "thread_ts", "reply_count", "reply_users_count"]
-    msg_list = {k:msg[k] for k in keys}
-    rply_list = from_msg_get_replies(msg)
 
-    return msg_list, rply_list
 
-def get_messages_from_channel(channel_path):
-    '''
-    get all the messages from a channel        
-    '''
-    channel_json_files = os.listdir(channel_path)
-    channel_msgs = [json.load(open(channel_path + "/" + f)) for f in channel_json_files]
-
-    df = pd.concat([pd.DataFrame(get_messages_dict(msgs)) for msgs in channel_msgs])
-    print(f"Number of messages in channel: {len(df)}")
     
-    return df
 
+    
 
-def convert_2_timestamp(column, data):
-    """convert from unix time to readable timestamp
-        args: column: columns that needs to be converted to timestamp
-                data: data that has the specified column
-    """
-    if column in data.columns.values:
-        timestamp_ = []
-        for time_unix in data[column]:
-            if time_unix == 0:
-                timestamp_.append(0)
-            else:
-                a = datetime.datetime.fromtimestamp(float(time_unix))
-                timestamp_.append(a.strftime('%Y-%m-%d %H:%M:%S'))
-        return timestamp_
-    else: print(f"{column} not in data")
+        
